19b6f97df5c9361a29b54e8bcbdfa6ba79629167
danielh1204 <80144195+danielh1204@users.noreply.github.com>
2023-07-29 14:06:51 +0800

    changing all sd to lowercase when reading nssfcfg.yaml (#12)
    * changing all sd to lowercase
    
    * update changeSdToLowercase

--- pkg/factory/factory.go
+++ pkg/factory/factory.go
@@ -7,6 +7,7 @@ package factory
 import (
 	"fmt"
 	"io/ioutil"
+	"strings"
 
 	"github.com/asaskevich/govalidator"
 	"gopkg.in/yaml.v2"
@@ -15,6 +16,66 @@ import (
 )
 
 var NssfConfig *Config
+
+func changeSdToLowercase(cfg *Config) {
+	// supportedNssaiInPlmnList
+	SupportedNssaiInPlmnList := cfg.Configuration.SupportedNssaiInPlmnList
+	for i := range SupportedNssaiInPlmnList {
+		SupportedSnssaiList := SupportedNssaiInPlmnList[i].SupportedSnssaiList
+		for j := range SupportedSnssaiList {
+			SupportedSnssaiList[j].Sd = strings.ToLower(SupportedSnssaiList[j].Sd)
+		}
+	}
+
+	// nsiList
+	NsiList := cfg.Configuration.NsiList
+	for i := range NsiList {
+		NsiList[i].Snssai.Sd = strings.ToLower(NsiList[i].Snssai.Sd)
+	}
+
+	// AmfSetList
+	AmfSetList := cfg.Configuration.AmfSetList
+	for i := range AmfSetList {
+		SupportedNssaiAvailabilityData := AmfSetList[i].SupportedNssaiAvailabilityData
+		for j := range SupportedNssaiAvailabilityData {
+			SupportedSnssaiList := SupportedNssaiAvailabilityData[j].SupportedSnssaiList
+			for k := range SupportedSnssaiList {
+				SupportedSnssaiList[k].Sd = strings.ToLower(SupportedSnssaiList[k].Sd)
+			}
+		}
+	}
+
+	// AmfList
+	AmfList := cfg.Configuration.AmfList
+	for i := range AmfList {
+		SupportedNssaiAvailabilityData := AmfList[i].SupportedNssaiAvailabilityData
+		for j := range SupportedNssaiAvailabilityData {
+			SupportedSnssaiList := SupportedNssaiAvailabilityData[j].SupportedSnssaiList
+			for k := range SupportedSnssaiList {
+				SupportedSnssaiList[k].Sd = strings.ToLower(SupportedSnssaiList[k].Sd)
+			}
+		}
+	}
+
+	// TaList
+	TaList := cfg.Configuration.TaList
+	for i := range TaList {
+		SupportedSnssaiList := TaList[i].SupportedSnssaiList
+		for j := range SupportedSnssaiList {
+			SupportedSnssaiList[j].Sd = strings.ToLower(SupportedSnssaiList[j].Sd)
+		}
+	}
+
+	// MappingListFromPlmn
+	MappingListFromPlmn := cfg.Configuration.MappingListFromPlmn
+	for i := range MappingListFromPlmn {
+		MappingOfSnssai := MappingListFromPlmn[i].MappingOfSnssai
+		for j := range MappingOfSnssai {
+			MappingOfSnssai[j].HomeSnssai.Sd = strings.ToLower(MappingOfSnssai[j].HomeSnssai.Sd)
+			MappingOfSnssai[j].ServingSnssai.Sd = strings.ToLower(MappingOfSnssai[j].ServingSnssai.Sd)
+		}
+	}
+}
 
 // TODO: Support configuration update from REST api
 func InitConfigFactory(f string, cfg *Config) error {
@@ -31,6 +92,7 @@ func InitConfigFactory(f string, cfg *Config) error {
 			return fmt.Errorf("[Factory] %+v", yamlErr)
 		}
 	}
+	changeSdToLowercase(cfg)
 
 	return nil
 }
