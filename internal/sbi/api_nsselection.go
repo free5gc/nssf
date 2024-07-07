e093e2a76c3f5fc8fb02b4e666b994fd11f91955
newb1er <michael.cs12@nycu.edu.tw>
2024-07-07 15:56:58 +0800

    refactor: move `bindInvalidParams` function into util package

--- internal/sbi/api_nsselection.go
+++ internal/sbi/api_nsselection.go
@@ -1,12 +1,10 @@
 package sbi
 
 import (
-	"fmt"
 	"net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 
 	"github.com/free5gc/nssf/internal/logger"
 	"github.com/free5gc/nssf/internal/sbi/processor"
@@ -34,36 +32,6 @@ func (s *Server) getNsSelectionRoutes() []Route {
 	}
 }
 
-func BindErrorInvalidParams(err error) []models.InvalidParam {
-	var invalidParams []models.InvalidParam
-	if errs, ok := err.(validator.ValidationErrors); ok {
-		for _, e := range errs {
-			ip := models.InvalidParam{
-				Param: e.Field(),
-			}
-
-			switch e.Tag() {
-			case "required":
-				ip.Reason = fmt.Sprintf("The `%s` field is required.", e.Field())
-			case "oneof":
-				ip.Reason = fmt.Sprintf("The `%s` field must be one of '%s'.", e.Field(), e.Param())
-			case "required_with":
-				ip.Reason = fmt.Sprintf("The `%s` field is required when `%s` is present.", e.Field(), e.Param())
-			case "required_without":
-				ip.Reason = fmt.Sprintf("The `%s` field is required when `%s` is not present.", e.Field(), e.Param())
-			default:
-				ip.Reason = fmt.Sprintf("Failed on the `%s` tag.", e.Tag())
-			}
-
-			invalidParams = append(invalidParams, ip)
-		}
-	} else {
-		logger.NsselLog.Errorf("Unknown error type: %+v", err)
-	}
-
-	return invalidParams
-}
-
 func (s *Server) NetworkSliceInformationGet(c *gin.Context) {
 	logger.NsselLog.Infof("Handle NSSelectionGet")
 
@@ -75,7 +43,7 @@ func (s *Server) NetworkSliceInformationGet(c *gin.Context) {
 			Status:        http.StatusBadRequest,
 			Detail:        err.Error(),
 			Instance:      "",
-			InvalidParams: BindErrorInvalidParams(err),
+			InvalidParams: util.BindErrorInvalidParamsMessages(err),
 		}
 		util.GinProblemJson(c, problemDetail)
 		return
